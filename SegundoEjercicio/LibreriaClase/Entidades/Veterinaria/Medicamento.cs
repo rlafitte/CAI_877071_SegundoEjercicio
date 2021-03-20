c34ebbe2b8d26d027c9905adea5dfd17546e66bf
Renard <rdlafitte@gmail.com>
2021-03-20 13:30:13 -0300

    Tratamiento
    Se agrega clase tratamiento

---
+++ SegundoEjercicio/LibreriaClase/Entidades/Veterinaria/Medicamento.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SegundoEjercicio.LibreriaClase.Entidades.Veterinaria
+{
+    class Medicamento
+    {
+        private string s_nombre;
+        private string s_marca;
+        private int i_stock;
+
+        public int i_Stock
+        {
+            get
+            {
+                return this.i_stock;
+            }
+            set
+            {
+                this.i_stock = value;
+            }
+        }
+        public string s_Marca
+        {
+            get
+            {
+                return this.s_marca;
+            }
+
+            set
+            {
+                this.s_marca = value;
+            }
+        }
+        public string s_Nombre
+        {
+            get
+            {
+                return this.s_nombre;
+            }
+            set
+            {
+                this.s_nombre = value;
+            }
+        }
+    }
+}
---
+++ SegundoEjercicio/LibreriaClase/Entidades/Veterinaria/Tratamiento.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SegundoEjercicio.LibreriaClase.Entidades.Veterinaria
+{
+    public class Tratamiento
+    {
+        private string s_tipo;
+        private string s_fechaTratamiento;
+        private string s_fechaAlta;
+        private string s_fechaDefuncion;
+        Animal[] animal;
+        Medicamento[] medicamento;
+    }
+}
