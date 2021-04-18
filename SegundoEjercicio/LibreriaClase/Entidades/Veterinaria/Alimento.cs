30a15dfe36c25d6f0686bae6d73c9a6a18a02624
Renard <rdlafitte@gmail.com>
2021-04-18 20:32:37 -0300

    Cambios definición

--- SegundoEjercicio/LibreriaClase/Entidades/Veterinaria/Alimento.cs
+++ SegundoEjercicio/LibreriaClase/Entidades/Veterinaria/Alimento.cs
@@ -8,85 +8,18 @@ namespace SegundoEjercicio.LibreriaClase.Entidades.Veterinaria
 {
     public class Alimento
     {
-        private string s_marca;
-        private string s_nombre;
-        private decimal d_peso;
-        private double d_precioVenta;
-        private double d_precioCompra;
-        private int i_stock;
-        
-        public int i_Stock
-        {
-            get
-            {
-                return this.i_stock;
-            }
-            set
-            {
-                this.i_stock = value;
-            }
-        }
-        public string s_Marca
-        {
-            get
-            {
-                return this.s_marca;
-            }
+        private string _marca;
+        private string _nombre;
+        private decimal _peso;
+        private double _precioVenta;
+        private double _precioCompra;
+        private int _stock;
 
-            set
-            {
-                this.s_marca = value;
-            }
-        }
-        public string s_Nombre
-        {
-            get
-            {
-                return this.s_nombre;
-            }
-            set
-            {
-                this.s_nombre = value;
-            }
-        }
-
-        public decimal d_Peso
-        {
-            get
-            {
-                return this.d_peso;
-            }
-            set
-            {
-                if (d_peso > 0) 
-                { 
-                this.d_peso = value;
-                }
-            }
-        }
-        public string s_PrecioVenta
-        {
-            get
-            {
-                return "$ " + this.d_precioVenta.ToString();
-            }
-
-            set
-            {
-                this.d_precioVenta = Convert.ToDouble(value);
-            }
-        }
-        public string s_PrecioCompra
-        {
-            get
-            {
-                return "$ " + this.d_precioCompra.ToString();
-            }
-
-            set
-            {
-                this.d_precioCompra = Convert.ToDouble(value);
-            }
-        }
+        public string S_marca { get => _marca; set => _marca = value; }
+        public string S_nombre { get => _nombre; set => _nombre = value; }
+        public decimal D_peso { get => _peso; set => _peso = value; }
+        public double D_precioVenta { get => _precioVenta; set => _precioVenta = value; }
+        public double D_precioCompra { get => _precioCompra; set => _precioCompra = value; }
+        public int I_stock { get => _stock; set => _stock = value; }
     }
 }
