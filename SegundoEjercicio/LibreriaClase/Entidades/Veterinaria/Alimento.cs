5c57c8b07eba08d322bca46030671952735b27b7
Renard <rdlafitte@gmail.com>
2021-04-18 20:34:24 -0300

    Cambios definición animal

--- SegundoEjercicio/LibreriaClase/Entidades/Veterinaria/Alimento.cs
+++ SegundoEjercicio/LibreriaClase/Entidades/Veterinaria/Alimento.cs
@@ -15,11 +15,11 @@ namespace SegundoEjercicio.LibreriaClase.Entidades.Veterinaria
         private double _precioCompra;
         private int _stock;
 
-        public string S_marca { get => _marca; set => _marca = value; }
-        public string S_nombre { get => _nombre; set => _nombre = value; }
-        public decimal D_peso { get => _peso; set => _peso = value; }
-        public double D_precioVenta { get => _precioVenta; set => _precioVenta = value; }
-        public double D_precioCompra { get => _precioCompra; set => _precioCompra = value; }
-        public int I_stock { get => _stock; set => _stock = value; }
+        public string Marca { get => _marca; set => _marca = value; }
+        public string Nombre { get => _nombre; set => _nombre = value; }
+        public decimal Peso { get => _peso; set => _peso = value; }
+        public double PrecioVenta { get => _precioVenta; set => _precioVenta = value; }
+        public double PrecioCompra { get => _precioCompra; set => _precioCompra = value; }
+        public int Stock { get => _stock; set => _stock = value; }
     }
 }
--- SegundoEjercicio/LibreriaClase/Entidades/Veterinaria/Animal.cs
+++ SegundoEjercicio/LibreriaClase/Entidades/Veterinaria/Animal.cs
@@ -8,98 +8,22 @@ namespace SegundoEjercicio.LibreriaClase.Entidades.Veterinaria
 {
     public class Animal
     {
-        private int i_identificacion;
-        private string s_nombre;
-        private string s_raza;
-        private string s_sexo;
-        private int i_edad;
-        private double d_peso;
-        private string s_enfermedades;
+        private int _identificacion;
+        private string _nombre;
+        private string _raza;
+        private string _sexo;
+        private int _edad;
+        private double _peso;
+        private string _enfermedades;
         private Cliente[] cliente;
 
-        public int i_Identificacion
-        {
-            get
-            {
-                return this.i_identificacion;
-            }
-            set
-            {
-                this.i_identificacion = value;
-            }
-        }
-
-        public string s_Nombre
-        {
-            get
-            {
-                return this.s_nombre;
-            }
-            set
-            {
-                this.s_nombre = value;
-            }
-        }
-
-        public string s_Raza
-        {
-            get
-            {
-                return this.s_raza;
-            }
-            set
-            {
-                this.s_raza = value;
-            }
-        }
-        public string s_Sexo
-        {
-            get
-            {
-                return this.s_sexo;
-            }
-            set
-            {
-                this.s_sexo = value;
-            }
-        }
-        public int i_Edad
-        {
-            get
-            {
-                return this.i_edad;
-            }
-            set
-            {
-                this.i_edad = value;
-            }
-        }
-        public Cliente Cliente
-        {
-            get;
-            set;
-        }
-        public string s_Peso
-        {
-            get
-            {
-                return this.d_peso.ToString();
-            }
-            set
-            {
-                this.d_peso = Convert.ToDouble(value);
-            }
-        }
-        public string s_Enfermedades
-        {
-            get
-            {
-                return this.s_enfermedades;
-            }
-            set
-            {
-                this.s_enfermedades = value;
-            }
-        }
+        public int Identificacion { get => _identificacion; set => _identificacion = value; }
+        public string Nombre { get => _nombre; set => _nombre = value; }
+        public string Raza { get => _raza; set => _raza = value; }
+        public string Sexo { get => _sexo; set => _sexo = value; }
+        public int Edad { get => _edad; set => _edad = value; }
+        public double Peso { get => _peso; set => _peso = value; }
+        public string Enfermedades { get => _enfermedades; set => _enfermedades = value; }
+        public Cliente[] Cliente { get => cliente; set => cliente = value; }
     }
 }
