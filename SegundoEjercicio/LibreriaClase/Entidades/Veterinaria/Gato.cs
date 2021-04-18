d5a0854c7fed0ed5249b597815e4edea81efc2ae
Renard <rdlafitte@gmail.com>
2021-04-18 20:38:04 -0300

    Clase Gato

---
+++ SegundoEjercicio/LibreriaClase/Entidades/Veterinaria/Gato.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SegundoEjercicio.LibreriaClase.Entidades.Veterinaria;
+
+namespace LibreriaClase.Entidades.Veterinaria
+{
+    public class Gato : Animal
+    {
+
+        public Gato() :base()
+        {
+
+        }
+    }
+}
